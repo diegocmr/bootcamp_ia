3c7724adb9dd13625c64c54c2b6729918b1fc4e2
Gabriel Victor <cl2003@fiap.com.br>
2023-03-15 23:47:39 -0300

    retirar teste de conexao

--- infra/api/app/app.py
+++ infra/api/app/app.py
@@ -6,19 +6,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    try:
-        cnx = mysql.connector.connect(
-            user='root', 
-            password='root',
-            host='bancodados',
-            database='mydb'
-        )
-    except Error as e:
-        print("Error while connecting to MySQL", e)
-    
-    pd.read_csv("/data/dadosclientes.csv")
-    print(pd[0])
-
     return '<p> Hello world </p>'
 
 if __name__ == '__main__':
