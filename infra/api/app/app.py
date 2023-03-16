43bc30d86129c8be5c6ea1190ecb0c9404e7d361
Gabriel Victor <cl2003@fiap.com.br>
2023-03-15 21:01:36 -0300

    Começar testes com o banco de dados

--- infra/api/app/app.py
+++ infra/api/app/app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+import mysql.connector
+from mysql.connector import Error
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    try:
+        cnx = mysql.connector.connect(
+            user='root', 
+            password='root',
+            host='bancodados',
+            database='mydb'
+        )
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    
+
     return '<p> Hello world </p>'
 
 if __name__ == '__main__':
-    app.run(port=8888,debug=True,host='0.0.0.0')
+    app.run(port=8080,debug=True,host='0.0.0.0')
