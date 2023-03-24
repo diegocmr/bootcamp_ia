4a92ff15dcaed0990fe62956404688dca516ce1f
Gabriel Victor <gabriel.silva@fiap.com.br>
2023-03-23 23:37:24 -0300

    Terminei a função de solicitar credito

--- infra/website/js/financeiro.js
+++ infra/website/js/financeiro.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    
+    $(".container-solicitar_emprestimo_resumo").hide();
+    $(".container-solicitar_emprestimo").hide();
     dataTableEmprestimos = $('#emprestimos').DataTable({
         language: PTBR,
         ajax: {
@@ -11,8 +12,18 @@ $(document).ready(function(){
         },       
         columns: [
             
-            { title: 'Valor Solicitado',data: 'valorSolicitado' },
-            { title: 'Valor Aprovado',data: 'valorAprovado' },
+            { title: 'Valor Solicitado',data: 'valorSolicitado',render:function(data, type, row){
+                if (data == "" || data == null ) {
+                    return "";
+                }                 
+                return "R$ "+ data;           
+            }},
+            { title: 'Valor Aprovado',data: 'valorAprovado',render:function(data, type, row){
+                if (data == "" || data == null ) {
+                    return "";
+                }                 
+                return "R$ "+ data;           
+            }},
             { type: "date-eu" ,title: 'Data', render:function(data, type, row){
                 if (row["dataAprovadoNivelAnalista"] == "" || row["dataAprovadoNivelAnalista"] == null ) {
                     return "";
@@ -26,23 +37,29 @@ $(document).ready(function(){
         order:[[3, 'desc']]
            
     });
+   
+    $("#tab-solicitar").click(function(){
+        var funcSucess = function(data) {
+            var credito = data["credito"];
+            if(credito) {            
+                $(".container-solicitar_emprestimo_resumo").show();
+                $(".container-solicitar_emprestimo").hide();            
+                $("#valor_credito_resumo").val("R$ " + credito["valorSolicitado"]);
+                $("#credito_status_resumo").val(credito["status"]);
+                $("#mensagem_credito_resumo").html(credito["mensagem"]);
+                return true;
+            }else{
+                $(".container-solicitar_emprestimo_resumo").hide();
+                $(".container-solicitar_emprestimo").show();   
+            }
+        };
+        var funcError = function(data) {
+            
+        };
     
-    var funcSucess = function(data) {
-        setMessage("Enviamos com sucesso sua solicitação!",".container-error",5000,"bg-success");  
-        dataTableEmprestimos.ajax.reload();
-    };
-    var funcError = function(data) {
-        setMessage("Ocorreu um erro, Tente novamente mais tarde",".container-error");  
-    };
-
-    var data = {
-        valorSolicitado,
-        mensagem
-    } 
-    post_api("cadastro_credito", data, funcSucess, funcError)
-
-
-
+        post_api("get_credito_analise", {}, funcSucess, funcError)
+    });
+    
     $('.enviar_solicitacao_credito').click(function(){
         var valorSolicitado = tratarFloat($("#valor_credito").val())
         var mensagem = $("#mensagem_credito").val()
@@ -58,6 +75,11 @@ $(document).ready(function(){
         }        
 
         var funcSucess = function(data) {
+            $(".container-solicitar_emprestimo_resumo").show();
+            $(".container-solicitar_emprestimo").hide();            
+            $("#valor_credito_resumo").val("R$ "+valorSolicitado);
+            $("#credito_status_resumo").val("emAnalise");
+            $("#mensagem_credito_resumo").html(mensagem);
             setMessage("Enviamos com sucesso sua solicitação!",".container-error",5000,"bg-success");  
             dataTableEmprestimos.ajax.reload();
         };
@@ -69,7 +91,7 @@ $(document).ready(function(){
             valorSolicitado,
             mensagem
         } 
-        
+
         post_api("cadastro_credito", data, funcSucess, funcError)
 
     });
